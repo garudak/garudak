eb1130f381022b36ff2322edc720497d4ee57838
Garuuda <60025072+Garuuda@users.noreply.github.com>
2020-11-30 20:58:05 -0500

    started the javascript for interacting with github api

--- app/static/repo_list.js
+++ app/static/repo_list.js
@@ -1,2 +1,23 @@
 // just get this shit to load some repos to a dict
+var debug = true;
 
+var github_api = 'https://api.github.com/users/garuuda';
+
+if(debug){
+    console.log("Script loaded?");
+}
+
+// to be called by request.onload only
+function dumpResponse() {
+    console.log(this.responseText);
+}
+
+function printRepoCount() {
+    var responseObj = JSON.parse(this.responseText);
+    console.log(responseObj.login + " has " + responseObj.public_repos + " public repo(s)!");
+}
+
+var request = new XMLHttpRequest();
+request.onload = dumpResponse;
+request.open('get', github_api + '/repos');    
+request.send();
